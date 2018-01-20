9fd1f0af3b43b865908bb3effa22cdaee0818b90
Benjamin Brunzel <benjamin.brunzel@iteratec.de>
2018-01-21 00:46:33 +0100

    Implement drive function

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import HackaTron
 
 main :: IO ()
 main = putStrLn "Hi, this is hackatron."
--- src/HackaTron.hs
+++ src/HackaTron.hs
@@ -1,7 +1,5 @@
 module HackaTron where
 
-import Data.Either
-
 -- A coordinate on the grid
 type Coordinate = (Int, Int)
 
@@ -34,22 +32,67 @@ data Turn = TurnLeft | TurnRight
 data Grid = Grid GridSize [Bike]
 
 data Death = Suicide Player
-           | Frag Player Player
+           --     Casulty Killer
+           | Frag Player  Player
            | Collision Player Player 
 
+play :: Grid -> [Move] -> (Grid, [Death])
+play = undefined
+
 drive :: Grid -> Bike -> Move -> Either Death Bike
-drive = undefined
+drive g b m = case death of
+    Just d -> Left d
+    Nothing -> Right $ driveBike b m
 
-isValidMove :: Grid -> Bike -> Move -> Bool
-isValidMove = undefined
-
-driveBike :: Bike -> Move -> Bike
-driveBike b m = Bike (player b) (newCourse m) newLocation newTrail
-    where newCourse (Steer t)  = turn t $ course b
-          newCourse Straight = course b
+    where newCord = stepCoordinate course' (currLocation b)
+          course' = newCourse (course b) m
+          getGridSize (Grid gs _ ) = gs
           
-          newLocation = stepCoordinate (newCourse m) (currLocation b)
+          death = getFirstJust $ (getWallKiss $ getGridSize g) : (getFrag g b m) : []
+
+          -- get death if out of grid
+          getWallKiss :: GridSize -> Maybe Death
+          getWallKiss gridSize = if newCord >= gridSize
+              then Just (Suicide (player b))
+              else Nothing
+
+-- returns the death caused by hitting a trail
+getFrag :: Grid -> Bike -> Move -> Maybe Death
+getFrag (Grid _ bikes) b m = case fragger of
+    Nothing -> Nothing
+    Just f -> Just $ getDeath (player f) (player b)
+
+    where fragger :: Maybe Bike
+          fragger = getFirstJust $ fmap (droveInTrail $ newCord) bikes
+
+          course' = newCourse (course b) m
+          newCord = stepCoordinate course' (currLocation b)
+
+          droveInTrail :: Coordinate -> Bike -> Maybe Bike
+          droveInTrail c bike =  if c `elem` (trail bike)
+              then Just bike
+              else Nothing
+
+          getDeath :: Player -> Player -> Death
+          getDeath p killer = if p == killer
+              then Suicide p
+              else Frag p killer
+
+getFirstJust :: [Maybe a] -> Maybe a
+getFirstJust [] = Nothing
+getFirstJust [x] = x
+getFirstJust ((Just x):_) = Just x
+getFirstJust (_:xs) = getFirstJust xs
+
+-- execute a single move
+driveBike :: Bike -> Move -> Bike
+driveBike b m = Bike (player b) (newCourse (course b) m) newLocation newTrail
+    where newLocation = stepCoordinate (newCourse (course b) m) (currLocation b)
           newTrail = (currLocation b) : (trail b)
+
+newCourse :: Course -> Move -> Course
+newCourse c Straight   = c
+newCourse c (Steer t)  = turn t $ c
 
 tupleApply :: (a -> c, b -> d) -> (a, b) -> (c, d)
 tupleApply (f, g) (a, b) = (f a, g b)
