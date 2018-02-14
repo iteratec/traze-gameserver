ab433c6fe0426828a621d228cdb63cd19d7d21f4
Benjamin Brunzel <benjamin.brunzel@iteratec.de>
2018-02-14 21:46:49 +0100

    Add yaml based configuration file

---
+++ app/Config.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+
+module Config where
+
+import Prelude hiding (lookup)
+import Data.Yaml.Config (load, subconfig, lookup)
+
+data Config = Config {
+  brokerHost :: String,
+  brokerPort :: Int,
+  clientName :: String
+}
+
+getConfig :: IO (Config)
+getConfig = do
+    config <- load "./traze.yaml"
+
+    mqttConfig <- subconfig "mqtt" config
+    host <- lookup "brokerHost" mqttConfig
+    port <- lookup "brokerPort" mqttConfig
+    name <- lookup "clientName" mqttConfig
+
+    return $ Config host port name
